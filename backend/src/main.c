704f84f43096d3e749683fa33e39f624a40a07fb
SimonFJ20 <simonfromjakobsen@gmail.com>
2026-03-20 13:25:26 +0100

    clang-format check

--- backend/src/main.c
+++ backend/src/main.c
@@ -1,63 +1,54 @@
-#include <string.h>
-#include <sys/types.h>
-#include <sys/select.h>
-#include <sys/socket.h>
 #include <microhttpd.h>
 #include <stdio.h>
+#include <string.h>
+#include <sys/select.h>
+#include <sys/socket.h>
+#include <sys/types.h>
 
 #define PORT 8888
 
 int print_out_key(
-    void *cls,
-    enum MHD_ValueKind kind, 
-    const char *key,
-    const char *value)
+    void* cls, enum MHD_ValueKind kind, const char* key, const char* value)
 {
-  printf ("%s: %s\n", key, value);
-  return MHD_YES;
+    printf("%s: %s\n", key, value);
+    return MHD_YES;
 }
 
-int answer_to_connection(
-    void *cls,
-    struct MHD_Connection *connection,
-    const char *url,
-    const char *method,
-    const char *version,
-    const char *upload_data,
-    size_t *upload_data_size,
-    void **req_cls)
+int answer_to_connection(void* cls,
+    struct MHD_Connection* connection,
+    const char* url,
+    const char* method,
+    const char* version,
+    const char* upload_data,
+    size_t* upload_data_size,
+    void** req_cls)
 {
-    const char *page  = "<html><body>Hello, browser!</body></html>";
-    struct MHD_Response *response;
+    const char* page = "<html><body>Hello, browser!</body></html>";
+    struct MHD_Response* response;
     int ret;
 
-    MHD_get_connection_values(
-        connection,
+    MHD_get_connection_values(connection,
         MHD_HEADER_KIND,
         (MHD_KeyValueIterator)&print_out_key,
         NULL);
 
-    printf ("New %s request for %s using version %s\n", method, url, version);
+    printf("New %s request for %s using version %s\n", method, url, version);
 
     response = MHD_create_response_from_buffer(
-        strlen(page),
-        (void*)page,
-        MHD_RESPMEM_PERSISTENT);
+        strlen(page), (void*)page, MHD_RESPMEM_PERSISTENT);
 
     ret = (int)MHD_queue_response(connection, MHD_HTTP_OK, response);
     MHD_destroy_response(response);
 
-  return ret;
+    return ret;
 }
-
 
 int main(void)
 {
     printf("hello\n");
-    struct MHD_Daemon *daemon;
+    struct MHD_Daemon* daemon;
 
-    daemon = MHD_start_daemon(
-        MHD_USE_INTERNAL_POLLING_THREAD,
+    daemon = MHD_start_daemon(MHD_USE_INTERNAL_POLLING_THREAD,
         PORT,
         NULL,
         NULL,
@@ -65,10 +56,10 @@ int main(void)
         NULL,
         MHD_OPTION_END);
 
-    if (NULL == daemon) return 1;
-        getchar();
+    if (NULL == daemon)
+        return 1;
+    getchar();
 
     MHD_stop_daemon(daemon);
     return 0;
 }
-
