bb6f74e3d23d9664fde346ffb4fbaa0b1a0f517c
sfja <sfja2004@gmail.com>
2026-03-23 23:02:28 +0100

    fix build

--- unnamed-game-engine/src/main.rs
+++ unnamed-game-engine/src/main.rs
@@ -2,10 +2,8 @@ mod engine;
 
 use crate::engine::game::Game;
 
-
 fn main() {
-    
-    let mut game = Game::new().unwrap();
-    game.run();
+    // let mut game: Game<(), _> = Game::new(todo!()).unwrap();
+    // game.run();
     println!("Hello, world!");
 }
