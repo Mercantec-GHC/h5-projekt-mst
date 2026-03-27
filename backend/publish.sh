83bc971f879d4685f553e5a64796d40fdec21619
sfja <sfja2004@gmail.com>
2026-03-27 18:19:50 +0100

    server deploy

---
+++ backend/publish.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+version=$1
+
+if [[ $version == "" ]]; then
+    echo "Please specify version"
+    echo "./publish.sh <version e.g. 1.0>"
+    exit 1
+fi
+
+tag=sfja/h5-mst-backend:$version
+
+set -xe
+
+docker build -t $tag .
+
+docker push $tag
+
